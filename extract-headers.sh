7a6f22ed2c7ceedeeb6d12de334a393a9245fad4
Sean Gilligan <sean@msgilligan.com>
2024-02-26 01:20:36 -0800

    Update jextract.sh for the new package location

--- extract-headers.sh
+++ extract-headers.sh
@@ -4,7 +4,7 @@ LIB_PKG="secp256k1-0.4.0"
 HASH="j9mf1fh4wbb8c3x1zwqfs218bhml1rbw"
 SECP_PATH=$NIX_STORE/$HASH-$LIB_PKG
 mkdir -p build
-jextract --target-package org.consensusj.secp256k1 \
+jextract --target-package org.bitcoinj.secp256k1.foreign.jextract \
         --output build \
         --source \
         -lsecp256k1 \
