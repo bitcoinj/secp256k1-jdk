4a82fd438577cae68e9db32c83c9c581577fa437
Sean Gilligan <sean@msgilligan.com>
2025-09-30 13:48:23 -0700

    secp-bitcoinj: remove direct dependencies on implementations
    use runtimOnly dependencies, instead.

--- secp-bitcoinj/src/main/java/module-info.java
+++ secp-bitcoinj/src/main/java/module-info.java
@@ -16,7 +16,6 @@
 @org.jspecify.annotations.NullMarked
 module org.bitcoinj.secp.bitcoinj {
     requires org.bitcoinj.secp.api;
-    requires org.bitcoinj.secp.ffm;
     requires org.jspecify;
     requires org.bitcoinj.core;
     requires org.bouncycastle.provider;
