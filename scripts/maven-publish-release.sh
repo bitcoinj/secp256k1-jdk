19766e4ac3d6320c15599cd7880ce737b4a22387
Sean Gilligan <sean@msgilligan.com>
2026-06-08 00:06:30 -0700

    maven-publish-release.sh, release-procedure.adoc: post-release update (for non dry-run)
    Update release procedure and script after first successful (non dry-run)
    publication.

--- scripts/maven-publish-release.sh
+++ scripts/maven-publish-release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Validate and publish release artifacts to our GitLab Maven artifact rpo
 # This should be run from the project root directory and inside the `nix develop` shell
+# JRELEASER_GITLAB_TOKEN should be set before running
 set -x
 export REVISION=$(mvn -q help:evaluate -Dexpression=revision -DforceStdout)
 export CI_REVISION=$(echo "$REVISION" | sed 's/-SNAPSHOT$/-ci/')
 
 export JRELEASER_PROJECT_VERSION="$CI_REVISION"
-export JRELEASER_GITLAB_TOKEN=dry-run
-jreleaser-cli deploy --dry-run
+jreleaser-cli deploy
