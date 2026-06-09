2dfc01cdaddfb237c39f80082f101113aef52f12
Sean Gilligan <sean@msgilligan.com>
2026-06-09 01:08:23 -0700

    maven-publish-release.sh: add --dry-run option as script parameter
    This simplifies testing the script.

--- scripts/maven-publish-release.sh
+++ scripts/maven-publish-release.sh
@@ -2,9 +2,15 @@
 # Validate and publish release artifacts to our GitLab Maven artifact rpo
 # This should be run from the project root directory and inside the `nix develop` shell
 # JRELEASER_GITLAB_TOKEN should be set before running
+if [[ $1 != "--dry-run" && $1 != "" ]]; then
+    echo "Usage: $0 [--dry-run]"
+    exit 1
+fi
+
 set -x
+OPTIONS=${1:-""}
 export REVISION=$(mvn -q help:evaluate -Dexpression=revision -DforceStdout)
 export CI_REVISION=$(echo "$REVISION" | sed 's/-SNAPSHOT$/-ci/')
 
 export JRELEASER_PROJECT_VERSION="$CI_REVISION"
-jreleaser-cli deploy
+jreleaser-cli deploy $OPTIONS
