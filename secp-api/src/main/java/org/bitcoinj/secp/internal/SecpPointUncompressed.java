714dc38414d8ea11e73b98fa7c87b1914046e7ab
Sean Gilligan <sean@msgilligan.com>
2026-08-08 07:58:51 -0700

    SecpPointUncompressed: remove unused static factory that takes ECPoint

--- secp-api/src/main/java/org/bitcoinj/secp/internal/SecpPointUncompressed.java
+++ secp-api/src/main/java/org/bitcoinj/secp/internal/SecpPointUncompressed.java
@@ -20,7 +20,6 @@ import org.bitcoinj.secp.SecpPoint;
 import org.jspecify.annotations.Nullable;
 
 import java.math.BigInteger;
-import java.security.spec.ECPoint;
 import java.util.Objects;
 
 /**
@@ -33,11 +32,6 @@ public class SecpPointUncompressed implements SecpPoint.Uncompressed {
     public SecpPointUncompressed(SecpFieldElement x, SecpFieldElement y) {
         this.x = x;
         this.y = y;
-    }
-
-    public static SecpPointUncompressed of(ECPoint point) {
-        return new SecpPointUncompressed(SecpFieldElement.of(point.getAffineX()),
-                SecpFieldElement.of(point.getAffineY()));
     }
 
     public static SecpPointUncompressed of(BigInteger x, BigInteger y) {
