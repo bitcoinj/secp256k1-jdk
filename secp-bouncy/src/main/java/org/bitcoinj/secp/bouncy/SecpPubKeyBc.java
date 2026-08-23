bitcoinj/secp256k1-jdk

commit 2e1a9207fc743518fd6ac59fe5e4e14d15a5dacd

SecpPubKeyBc: Bouncy Castle implementation of SecpPubKey

secp-bouncy/src/main/java/org/bitcoinj/secp/bouncy/SecpPubKeyBc.java
@@ -1,0 +1,150 @@
+/*
+ * Copyright 2023-2026 secp256k1-jdk Developers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bitcoinj.secp.bouncy;
+
+import org.bitcoinj.secp.SecpFieldElement;
+import org.bitcoinj.secp.SecpPoint;
+import org.bitcoinj.secp.SecpPubKey;
+import org.bitcoinj.secp.internal.ByteUtils;
+import org.bitcoinj.secp.internal.SecpECPoint;
+import org.bitcoinj.secp.internal.SecpPointCompressed;
+import org.bouncycastle.math.ec.custom.sec.SecP256K1Point;
+import org.jspecify.annotations.Nullable;
+
+import java.security.spec.ECPoint;
+import java.util.Objects;
+
+/**
+ * Bouncy Castle implementation of SecpPubKey using {@link SecP256K1Point}.
+ */
+class SecpPubKeyBc implements SecpPubKey {
+    private final SecP256K1Point bcPoint;
+
+    SecpPubKeyBc(SecP256K1Point bcPoint) {
+        this.bcPoint = bcPoint;
+    }
+
+    static SecpPubKeyBc of(org.bouncycastle.math.ec.ECPoint bcPoint) {
+        if (bcPoint.isInfinity()) { throw new IllegalArgumentException("bcPoint is infinity"); }
+        if (bcPoint instanceof SecP256K1Point) {
+            return new SecpPubKeyBc((SecP256K1Point) bcPoint);
+        } else {
+            throw new IllegalArgumentException("point must be SecP256K1Point");
+        }
+    }
+
+    @Override
+    public ECPoint getW() {
+        org.bouncycastle.math.ec.ECPoint normalized = bcPoint.normalize();
+        return SecpECPoint.of(normalized.getAffineXCoord().toBigInteger(), normalized.getAffineYCoord().toBigInteger());
+    }
+
+    /**
+     * Return Bouncy Castle native secp256k1 point
+     * @return secp256k1 point
+     */
+    SecP256K1Point getQ() {
+        return bcPoint;
+    }
+
+    @Override
+    public Uncompressed point() {
+        return this;
+    }
+
+    @Override
+    public SecpFieldElement x() {
+        org.bouncycastle.math.ec.ECPoint normalized = bcPoint.normalize();
+        return SecpFieldElement.of(normalized.getAffineXCoord().getEncoded());
+    }
+
+    @Override
+    public SecpFieldElement y() {
+        org.bouncycastle.math.ec.ECPoint normalized = bcPoint.normalize();
+        return SecpFieldElement.of(normalized.getAffineYCoord().getEncoded());
+    }
+
+    @Override
+    public Compressed compress() {
+        return new SecpPointCompressed(x(), y());
+    }
+
+    @Override
+    public String getFormat() {
+        return COMPRESSED_FORMAT_NAME;
+    }
+
+    @Override
+    public byte[] serialize(boolean compressed) {
+        return compressed
+                ? SecpPubKeyBc.serializeCompressed(bcPoint)
+                : serializeUncompressed(bcPoint);
+    }
+
+    /**
+     * {@inheritDoc}
+     * Note: Public Keys are stored uncompressed, but their default serialization is compressed.
+     * @return
+     */
+    @Override
+    public byte[] serialize() {
+        return serializeCompressed(bcPoint);
+    }
+
+    static byte[] serializeUncompressed(SecP256K1Point bcPoint) {
+        org.bouncycastle.math.ec.ECPoint norm = bcPoint.normalize();
+        return SecpPoint.serializeUncompressed(norm.getAffineXCoord().getEncoded(), norm.getAffineYCoord().getEncoded());
+    }
+
+    static byte[] serializeCompressed(SecP256K1Point bcPoint) {
+        org.bouncycastle.math.ec.ECPoint norm = bcPoint.normalize();
+        return SecpPoint.serializeCompressed(norm.getAffineXCoord().getEncoded(), isOdd(norm));
+    }
+
+    @Override
+    public boolean isOdd() {
+        return SecpPubKeyBc.isOdd(bcPoint.normalize());
+    }
+
+    private static boolean isOdd(org.bouncycastle.math.ec.ECPoint normalizePoint) {
+        return normalizePoint.getAffineYCoord().testBitZero();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (!(o instanceof SecpPoint.Uncompressed)) return false;
+        if (o instanceof SecpPubKeyBc) {
+            SecpPubKeyBc other = (SecpPubKeyBc) o;
+            return bcPoint.equals(other.bcPoint);
+        } else {
+            SecpPoint.Uncompressed other = (SecpPoint.Uncompressed) o;
+            org.bouncycastle.math.ec.ECPoint norm = bcPoint.normalize();
+            return Objects.equals(norm.getAffineXCoord().toBigInteger(), other.x().toBigInteger()) &&
+                    Objects.equals(norm.getAffineYCoord().toBigInteger(), other.y().toBigInteger());
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        org.bouncycastle.math.ec.ECPoint norm = bcPoint.normalize();
+        return Objects.hash(norm.getAffineXCoord(), norm.getAffineYCoord());
+    }
+
+    @Override
+    public String toString() {
+        return ByteUtils.toHexString(this.serialize());
+    }
+}

secp-api/src/main/java/org/bitcoinj/secp/internal/SecpECPoint.java
@@ -45,6 +45,12 @@ public class SecpECPoint extends ECPoint implements SecpPoint.Uncompressed {
         return new SecpECPoint(point.x().toBigInteger(), point.y().toBigInteger());
     }
 
+    // Unvalidated, assumes caller has validated the point.
+    // We should have implementation-specific versions/subclasses of this type (make it abstract)
+    public static SecpECPoint of(BigInteger x, BigInteger y) {
+        return new SecpECPoint(x, y);
+    }
+
     @Override
     public SecpFieldElement x() {
         return SecpFieldElement.of(super.getAffineX());

secp-bouncy/src/main/java/org/bitcoinj/secp/bouncy/BC.java
@@ -16,10 +16,8 @@
 package org.bitcoinj.secp.bouncy;
 
 import org.bitcoinj.secp.SecpPoint;
-import org.bitcoinj.secp.SecpPubKey;
-import org.bitcoinj.secp.internal.SecpPointUncompressed;
-import org.bitcoinj.secp.internal.SecpPubKeyImpl;
 import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.math.ec.custom.sec.SecP256K1Point;
 
 import static org.bitcoinj.secp.bouncy.Bouncy256k1.BC_CURVE;
 
@@ -28,19 +26,17 @@ import static org.bitcoinj.secp.bouncy.Bouncy256k1.BC_CURVE;
  */
 interface BC {
 
-    static SecpPubKey toSecpPubKey(ECPoint bcPoint) {
-        if (bcPoint.isInfinity()) { throw new IllegalArgumentException("bcPoint is infinity"); }
-        return  new SecpPubKeyImpl(BC.toSecpPoint(bcPoint));
+    static SecpPubKeyBc toSecpPubKey(ECPoint bcPoint) {
+        return SecpPubKeyBc.of(bcPoint);
     }
 
-    static SecpPointUncompressed toSecpPoint(ECPoint bcPoint) {
-        if (bcPoint.isInfinity()) { throw new IllegalArgumentException("bcPoint is infinity"); }
-        return SecpPointUncompressed.of(
-                bcPoint.normalize().getAffineXCoord().toBigInteger(),
-                bcPoint.normalize().getAffineYCoord().toBigInteger());
+    static SecpPubKeyBc toSecpPoint(ECPoint bcPoint) {
+        return SecpPubKeyBc.of(bcPoint);
     }
 
-    static ECPoint fromSecpPoint(SecpPoint.Uncompressed point) {
-        return BC_CURVE.createPoint(point.x().toBigInteger(), point.y().toBigInteger());
+    static SecP256K1Point fromSecpPoint(SecpPoint.Uncompressed point) {
+        return point instanceof SecpPubKeyBc
+                ? ((SecpPubKeyBc) point).getQ()
+                : (SecP256K1Point) BC_CURVE.createPoint(point.x().toBigInteger(), point.y().toBigInteger());
     }
 }

secp-bouncy/src/main/java/org/bitcoinj/secp/bouncy/Bouncy256k1.java
@@ -26,7 +26,6 @@ import org.bitcoinj.secp.SchnorrSignature;
 import org.bitcoinj.secp.Secp256k1;
 import org.bitcoinj.secp.EcdsaSignature;
 import org.bitcoinj.secp.internal.EcdhSharedSecretImpl;
-import org.bitcoinj.secp.internal.EcdsaSignatureImpl;
 import org.bitcoinj.secp.internal.SchnorrSignatureImpl;
 import org.bitcoinj.secp.internal.SecpKeyPairImpl;
 import org.bitcoinj.secp.internal.SecpPrivKeyImpl;
@@ -49,6 +48,7 @@ import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.math.ec.FixedPointCombMultiplier;
 import org.bouncycastle.math.ec.FixedPointUtil;
 import org.bouncycastle.math.ec.custom.sec.SecP256K1Curve;
+import org.bouncycastle.math.ec.custom.sec.SecP256K1Point;
 import org.bouncycastle.util.Arrays;
 
 import java.math.BigInteger;
@@ -130,7 +130,7 @@ public class Bouncy256k1 implements Secp256k1 {
 
     @Override
     public SecpPubKey ecPubKeyCreate(SecpPrivKey privKey) {
-        ECPoint pub = BC_ECDOMAIN_PARAMS.getG().multiply(privKey.getS()).normalize();
+        ECPoint pub = BC_ECDOMAIN_PARAMS.getG().multiply(privKey.getS());
         return BC.toSecpPubKey(pub);
     }
 
@@ -162,15 +162,15 @@ public class Bouncy256k1 implements Secp256k1 {
 
     @Override
     public SecpPubKey ecPubKeyTweakMul(SecpPoint.Uncompressed pubKey, BigInteger scalarMultiplier) {
-        ECPoint pubKeyBC = BC_CURVE.createPoint(pubKey.x().toBigInteger(), pubKey.y().toBigInteger());
-        ECPoint pub = new FixedPointCombMultiplier().multiply(pubKeyBC, scalarMultiplier).normalize();
+        SecP256K1Point pubKeyBC = BC.fromSecpPoint(pubKey);
+        ECPoint pub = new FixedPointCombMultiplier().multiply(pubKeyBC, scalarMultiplier);
         return BC.toSecpPubKey(pub);
     }
 
     @Override
     public SecpPubKey ecPubKeyCombine(SecpPoint.Uncompressed  key1, SecpPoint.Uncompressed  key2) {
-        ECPoint pubKey1BC = BC_CURVE.createPoint(key1.x().toBigInteger(), key1.y().toBigInteger());
-        ECPoint pubKey2BC = BC_CURVE.createPoint(key2.x().toBigInteger(), key2.y().toBigInteger());
+        SecP256K1Point pubKey1BC = BC.fromSecpPoint(key1);
+        SecP256K1Point pubKey2BC = BC.fromSecpPoint(key2);
         ECPoint result = pubKey1BC.add(pubKey2BC);
         return BC.toSecpPubKey(result);
     }
@@ -283,7 +283,7 @@ public class Bouncy256k1 implements Secp256k1 {
         checkArg(msg_hash_data.length == 32, "Message must be 32-byte (hash)");
         if (!signature.hasLowS()) return SecpResult.ok(false);
         ECDSASigner signer = new ECDSASigner();
-        ECPoint pubPoint = BC.fromSecpPoint(pubKey.point());
+        SecP256K1Point pubPoint = BC.fromSecpPoint(pubKey);
         ECPublicKeyParameters params = new ECPublicKeyParameters(pubPoint, BC_ECDOMAIN_PARAMS);
         signer.init(false, params);
         boolean result;
@@ -381,20 +381,20 @@ public class Bouncy256k1 implements Secp256k1 {
 
     @Override
     public SecpResult<EcdhSharedSecret> ecdh(SecpPubKey pubKey, SecpPrivKey privKey) {
-        ECPoint point = BC.fromSecpPoint(pubKey.point());
-        ECPoint ssPoint = point.multiply(privKey.getS()).normalize();
-        byte[] hashed = ecdhHash(BC.toSecpPubKey(ssPoint));
+        SecP256K1Point point = BC.fromSecpPoint(pubKey);
+        SecP256K1Point ssPoint = (SecP256K1Point) point.multiply(privKey.getS());
+        byte[] hashed = ecdhHash(ssPoint);
         return SecpResult.ok(new EcdhSharedSecretImpl(hashed));
     }
 
-    private byte[] ecdhHash(SecpPubKey sspk) {
+    private byte[] ecdhHash(SecP256K1Point point) {
         MessageDigest digest;
         try {
             digest = MessageDigest.getInstance("SHA-256");
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);  // Can't happen.
         }
-        digest.update(sspk.serialize(true));
+        digest.update(SecpPubKeyBc.serializeCompressed(point));
         return digest.digest();
     }
 

secp-bouncy/src/test/java/org/bitcoinj/secp/bouncy/BouncyPubKeyTest.java
@@ -15,12 +15,16 @@
  */
 package org.bitcoinj.secp.bouncy;
 
+import org.bitcoinj.secp.Secp256k1;
 import org.bitcoinj.secp.SecpPoint;
+import org.bitcoinj.secp.internal.SecpPointUncompressed;
+import org.bouncycastle.math.ec.custom.sec.SecP256K1Point;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *  Tests for BouncyPubKey.
@@ -36,11 +40,25 @@ public class BouncyPubKeyTest {
             point = secp.ecPubKeyCreate(secp.ecPrivKeyCreate()).point();
         }
         assertNotNull(point);
-        org.bouncycastle.math.ec.ECPoint bcPoint = BC.fromSecpPoint(point);
+        SecP256K1Point bcPoint = (SecP256K1Point) BC.fromSecpPoint(point).normalize();
 
         assertNotNull(bcPoint);
         assertEquals(point.x().toBigInteger(), bcPoint.getAffineXCoord().toBigInteger());
         assertEquals(point.y().toBigInteger(), bcPoint.getAffineYCoord().toBigInteger());
+    }
+
+    @Test
+    public void testEquals() {
+        SecpPubKeyBc g1 = BC.toSecpPoint(Bouncy256k1.BC_ECDOMAIN_PARAMS.getG());
+        assertEquals(Secp256k1.G.x(), g1.x());
+        assertEquals(Secp256k1.G.y(), g1.y());
+
+        SecpPubKeyBc g2 = new SecpPubKeyBc(g1.getQ());
+        assertEquals(g1, g2);
+
+        SecpPointUncompressed g3 = SecpPointUncompressed.of(g1.x().toBigInteger(), g1.y().toBigInteger());
+        assertEquals(g1, g3);   // Test SecpPubKeyBc.equals()
+        assertEquals(g3, g1);   // Test SecpPointUncompressed.equals()
     }
 
     @Test

commit b9d4ac5210b0131fcc26c1437bbe0155e28ff1a7

SecpPubKeyBc: Fix toString() method and BROKEN BUILD
The build was broken 2 commits back in 2e1a9207fc743518fd6ac59fe5e4e14d15a5dacd
and for some reason the merge queue merged in anyways.

This commit fixes the issue.

secp-bouncy/src/main/java/org/bitcoinj/secp/bouncy/SecpPubKeyBc.java
@@ -18,9 +18,9 @@ package org.bitcoinj.secp.bouncy;
 import org.bitcoinj.secp.SecpFieldElement;
 import org.bitcoinj.secp.SecpPoint;
 import org.bitcoinj.secp.SecpPubKey;
-import org.bitcoinj.secp.internal.ByteUtils;
 import org.bitcoinj.secp.internal.SecpECPoint;
 import org.bitcoinj.secp.internal.SecpPointCompressed;
+import org.bitcoinj.secp.internal.UInt256;
 import org.bouncycastle.math.ec.custom.sec.SecP256K1Point;
 import org.jspecify.annotations.Nullable;
 
@@ -145,6 +145,6 @@ class SecpPubKeyBc implements SecpPubKey {
 
     @Override
     public String toString() {
-        return ByteUtils.toHexString(this.serialize());
+        return UInt256.toHexString(this.serialize());
     }
 }